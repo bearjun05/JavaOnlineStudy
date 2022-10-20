b52f5114cd567a0615e415589d885ad7c0f4d8c1
bearjun05 <bearjun05@naver.com>
2022-10-20 19:46:12 +0900

    init commit

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,27 @@
 public class Account {
     //작성 위치
+
+    private String accountNumber;
+    private String name;
+    private int money;
+
+    public Account() {
+
+    }
+    public Account (String accountNumber, String name, int money){
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.money = money;
+    }
+
+
+    public String getAccountNumber(){return accountNumber;}
+    public void setAccountNumber(String accountNumber){this.accountNumber = accountNumber;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public int getMoney() {return money;}
+    public void setMoney(int money) {this.money = money;}
+
+
+
 }
--- src/BankApplication.java
+++ src/BankApplication.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class BankApplication {
     private static Account[] account = new Account[100];
     private static Scanner scanner = new Scanner(System.in);
+    public static int cnt = -1;
+
 
 
     public static void main(String[] args) {
@@ -33,20 +35,67 @@ public class BankApplication {
     //계좌생성하기
     private static void createAccount() {
         // 작성 위치
+        cnt++;
+        System.out.print("계좌번호 : ");
+        String accountNumber = scanner.next();
+        System.out.print("계좌주 : ");
+        String name = scanner.next();
+        System.out.print("초기입금액 : ");
+        int money = scanner.nextInt();
+
+        account[cnt] = new Account(accountNumber, name, money);
+
+//        Account account11 = new Account(accountNumber, name, money);
+//
+//        Account account1 = new Account();
+//        account1.setAccountNumber(accountNumber);
+//        account1.setName(name);
+//        account1.setMoney(money);
     }
+
 
     //계좌목록보기
     private static void accountList() {
         // 작성 위치
+        for (int i = 0; i <= cnt; i++) {
+            System.out.println(account[i].getAccountNumber()+" "
+                                +account[i].getName()+" "
+                                +account[i].getMoney());
+        }
     }
 
     //예금하기
     private static void deposit() {
       // 작성 위치
+        System.out.print("계좌번호 : ");
+        String accountNumber = scanner.next();
+        System.out.print("예금액 : ");
+        int money = scanner.nextInt();
+        for (int i = 0; i <= cnt; i++) {
+            if(account[i].getAccountNumber().equals(accountNumber)){
+                account[i].setMoney(account[i].getMoney()+money);
+            }
+        }
     }
 
     //출금하기
     private static void withdraw() {
       // 작성 위치
+        System.out.print("계좌번호 : ");
+        String accountNumber = scanner.next();
+        System.out.print("출금액 : ");
+        int money = scanner.nextInt();
+
+        for (int i = 0; i <= cnt; i++) {
+            if(account[i].getAccountNumber().equals(accountNumber)){
+                if(account[i].getMoney()<money){
+                    System.out.println("계좌 잔액이 부족합니다.");
+                    return;
+                }
+                account[i].setMoney(account[i].getMoney()-money);
+            }
+        }
+
+
     }
 }
